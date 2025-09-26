7113332ff865392ec1746e9ce232894fc494d6db
SANDIPAN JHA <sandipanjha3@gmail.com>
2025-09-26 12:14:37 +0530

    Fix: mail send fixed using brevo

--- services/emailService.js
+++ services/emailService.js
@@ -3,12 +3,12 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
+  host: "smtp-relay.brevo.com",
+  port: "587",
   secure: false,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: "97e69c001@smtp-brevo.com", // Will use '97e69c001@smtp-brevo.com'
+    pass: "MdEGkBPJN9qnLAOh", // Will use your Master Password
   },
 });
 
