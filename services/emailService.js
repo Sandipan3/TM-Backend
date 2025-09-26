e1eda76d5aa9f52b3658af3529f509857e6f9028
SANDIPAN JHA <sandipanjha3@gmail.com>
2025-09-26 11:56:13 +0530

    Fix: mail send fixed

--- services/emailService.js
+++ services/emailService.js
@@ -3,7 +3,9 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: process.env.SMTP_SERVICE,
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
